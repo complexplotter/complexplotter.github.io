acc466c1d5d9effaab2f54f1342748ae9fa647e6
Aeneas <134146343+Nighty39@users.noreply.github.com>
2025-11-30 21:04:14 +0100

    multiple functions at the same time

--- src/main.js
+++ src/main.js
@@ -76,6 +76,7 @@ const functionText = document.getElementById('function-textbox');
 const gridSizeText = document.getElementById('gridsize-textbox');
 const resolutionText = document.getElementById('resolution-textbox');
 const settings = document.getElementById('settings-menu');
+const inputs = document.getElementById('input-menu');
 
 let gridSize = 8;
 let resolution = 64;
@@ -109,34 +110,60 @@ scene.add(xAxis, zAxis);
 xAxis.rotateZ(Math.PI/2);
 zAxis.rotateX(Math.PI/2);
 
+function storeInput(planeCounter, expression) {
+    const box = document.createElement('div');
+    box.textContent = String.fromCharCode(96+planeCounter).concat('(z) = ').concat(expression).concat(' ');
+    box.id = planeCounter;
+    box.className = 'input-box';
+    const closeButton = document.createElement('button')
+    closeButton.textContent = 'X';
+    closeButton.className = 'close-button';
+    closeButton.addEventListener('click', (event) => {
+        removePlane(parseInt(box.id));
+    });
+    box.appendChild(closeButton);
+    return box;
+}
 
-function getColor(im) {
+const loadedPlanes = [];
+const scope = new Map();
+const planeFunctions = new Map();
+let planeCounter = 0;
+
+function getColor(im, planeCounter) {
     const color = new THREE.Color();
     const colorneg = new THREE.Color(0x0000ff);
     const colorpos = new THREE.Color(0xff0000);
+    /*let modulo = planeCounter % 2;
+    switch(modulo) {
+        case 0:
+            colorneg.set(new THREE.Color(0x0000ff));
+            colorpos.set(new THREE.Color(0xff0000));
+        case 1:
+            colorneg.set(new THREE.Color(0x00ffff));
+            colorpos.set(new THREE.Color(0xffff00));
+    }*/
     const c = 1;
     colorneg.set(colorneg.r*c*-im/gridSize, colorneg.g*c*-im/gridSize, colorneg.b*c*-im/gridSize);
     colorpos.set(colorpos.r*c*im/gridSize, colorpos.g*c*im/gridSize, colorpos.b*c*im/gridSize);
     return im <= 0 ? color.set(colorneg) : color.set(colorpos);
 }
 
-
-
-
-const loadedPlanes = [];
-let planeCounter = 0;
-
-function removePlane(name) {
-    const removedPlane = scene.getObjectByName(`ComplexPlane${name}`);
+function removePlane(id) {
+    const box = document.getElementById(id);
+    box.remove();
+    const removedPlane = scene.getObjectByName(`ComplexPlane${id}`);
+    const name = String.fromCodePoint(96 + id);
     if(removedPlane) {
+        scope.delete(name); 
         scene.remove(removedPlane);
         removedPlane.geometry.dispose();
+        loadedPlanes.splice(loadedPlanes.indexOf(removedPlane), 1);
     }
 }
 
 function load() {
     planeCounter++;
-    removePlane(planeCounter-1);
     const planeGeometry = new THREE.PlaneGeometry(gridSize, gridSize, resolution, resolution);
     const planeMaterial = new THREE.MeshBasicMaterial({vertexColors: true, wireframe: false, side: THREE.DoubleSide});
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
@@ -144,6 +171,13 @@ function load() {
     plane.name = planeName;
     scene.add(plane);
     loadedPlanes.push(plane);
+    alert(planeCounter);
+    
+    const newBox = storeInput(planeCounter, functionText.value);
+    inputs.appendChild(newBox);
+
+    planeFunctions.set(planeCounter, functionText.value);
+    
     
     plane.rotation.x = -Math.PI/2;
     const positionAttribute = planeGeometry.getAttribute('position');
@@ -153,12 +187,12 @@ function load() {
     for (let i = 0; i < positionAttribute.count; i++) {
 	    vertex.fromBufferAttribute(positionAttribute, i);
         const z = math.complex(vertex.x, vertex.y);
-        const scope = {
-            z: z,
-        };
+        const func = math.evaluate(String.fromCodePoint(96+planeCounter).concat('(z) = ').concat(functionText.value))
+        scope.set('z', z);
+        scope.set(String.fromCodePoint(96+planeCounter), func);
         const result = math.evaluate(functionText.value, scope);
 	    vertex.z = math.re(result);
-        const color = getColor(math.im(result));
+        const color = getColor(math.im(result), planeCounter);
         colors[colorIndex++] = color.r;
         colors[colorIndex++] = color.g;
         colors[colorIndex++] = color.b;
@@ -166,7 +200,47 @@ function load() {
 	    positionAttribute.setXYZ(i, vertex.x, vertex.y, vertex.z);
         planeGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
     }
-    textbox.value = "";
+    functionText.value = "";
+}
+
+function reload() {
+    const oldPlanes = loadedPlanes;
+    alert('length:'.concat(oldPlanes.length));
+    oldPlanes.forEach(element => {
+        const id = parseInt(element.name.replace(/[^\d]/g, ''));
+        removePlane(id);
+        const planeGeometry = new THREE.PlaneGeometry(gridSize, gridSize, resolution, resolution);
+        const planeMaterial = new THREE.MeshBasicMaterial({vertexColors: true, wireframe: false, side: THREE.DoubleSide});
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        const planeName = element.name;
+        plane.name = planeName;
+        scene.add(plane);
+        loadedPlanes.splice(id-1, 0, plane);
+        const func = planeFunctions.get(id);
+    
+        const newBox = storeInput(id, func);
+        inputs.appendChild(newBox);
+    
+        plane.rotation.x = -Math.PI/2;
+        const positionAttribute = planeGeometry.getAttribute('position');
+        const vertex = new THREE.Vector3();
+        const colors = [];
+        let colorIndex = 0;
+        for (let i = 0; i < positionAttribute.count; i++) {
+	        vertex.fromBufferAttribute(positionAttribute, i);
+            const z = math.complex(vertex.x, vertex.y);
+            scope.set('z', z);
+            const result = math.evaluate(func, scope);
+	        vertex.z = math.re(result);
+            const color = getColor(math.im(result), id);
+            colors[colorIndex++] = color.r;
+            colors[colorIndex++] = color.g;
+            colors[colorIndex++] = color.b;
+
+	        positionAttribute.setXYZ(i, vertex.x, vertex.y, vertex.z);
+            planeGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    }
+    });
 }
 
 btn1.addEventListener('click', load);
@@ -177,30 +251,35 @@ functionText.addEventListener('keyup', (event) => {
     }
 });
 
-
+gridSizeText.addEventListener('blur', (event) => {
+    gridSize = gridSizeText.value;
+    scene.remove(xAxis, zAxis, gridHelper);
+    axisGeometry = new THREE.CylinderGeometry(0.02, 0.02, gridSize, 10, 1, false);
+    xAxis = new THREE.Mesh(axisGeometry, axisMaterial);
+    zAxis = new THREE.Mesh(axisGeometry, axisMaterial);
+    xAxis.rotateZ(Math.PI/2);
+    zAxis.rotateX(Math.PI/2);
+    gridHelper = new THREE.GridHelper(gridSize, gridSize);
+    scene.add(xAxis, zAxis, gridHelper);
+    reload();
+});
 gridSizeText.addEventListener('keyup', (event) => {
     if(event.key=='Enter') {
         gridSizeText.blur();
-        gridSize = gridSizeText.value;
-        scene.remove(xAxis, zAxis, gridHelper);
-        axisGeometry = new THREE.CylinderGeometry(0.02, 0.02, gridSize, 10, 1, false);
-        xAxis = new THREE.Mesh(axisGeometry, axisMaterial);
-        zAxis = new THREE.Mesh(axisGeometry, axisMaterial);
-        xAxis.rotateZ(Math.PI/2);
-        zAxis.rotateX(Math.PI/2);
-        gridHelper = new THREE.GridHelper(gridSize, gridSize);
-        scene.add(xAxis, zAxis, gridHelper);
-        load();
     }
 });
 
+
+resolutionText.addEventListener('blur', (event) => {
+    resolution = resolutionText.value;
+    reload();
+});
 resolutionText.addEventListener('keyup', (event) => {
     if(event.key=='Enter') {
         resolutionText.blur();
-        resolution = resolutionText.value;
-        load();
     }
 });
+
 
 function showSettings(){
     settings.hidden = !settings.hidden;
